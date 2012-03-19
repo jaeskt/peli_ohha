381f54ac3fb76d167efc9b5d5028de59c7fd8528
Timo A Jaaskelainen <timojaas@dhp-27.cs.helsinki.fi>
2012-03-19 12:36:33 +0200

    testi

---
+++ kaksintaistelu/src/kaksintaistelu/Kaksintaistelu.java
@@ -0,0 +1,10 @@
+package kaksintaistelu;
+
+
+public class Kaksintaistelu {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
---
+++ kaksintaistelu/src/kaksintaistelu/kortti.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package kaksintaistelu;
+
+/**
+ *
+ * @author timojaas
+ */
+// Timo Jääskeläinen Tehtävä #.# Viikko #
+public class kortti {
+fgdgdfgdfgdfgdg
+}
