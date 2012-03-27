418ce03826da1136bb9afc8e3c6b90bc908b5146
Timo A Jaaskelainen <timojaas@dhp-27.cs.helsinki.fi>
2012-03-27 15:29:45 +0300

    päivitys

--- kaksintaistelu/src/kaksintaistelu/Kaksintaistelu.java
+++ kaksintaistelu/src/kaksintaistelu/Kaksintaistelu.java
@@ -1,10 +1,20 @@
 package kaksintaistelu;
 
+import java.awt.*;
+import javax.swing.*;
 
 public class Kaksintaistelu {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        JFrame ikkuna = new JFrame();
+        ikkuna.setSize(800, 800);
+        ikkuna.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        ikkuna.setVisible(true);
+        grafiikka peli = new grafiikka();
+        peli.setBackground(Color.BLACK);
+        Container sisalto = ikkuna.getContentPane();
+        sisalto.add(peli);
+        
+        
     }
-
 }
--- kaksintaistelu/src/kaksintaistelu/grafiikka.java
+++ kaksintaistelu/src/kaksintaistelu/grafiikka.java
@@ -2,15 +2,29 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package kaksintaistelu;
 
 /**
  * @author timojaas
  * @version 0.02
  * @since 21-3-2012
+ * 
+ * Grafiikka luokka huolehtii kaiken piirtämisen mitä pelissä tarvitaan.
+ * 
  */
 // Timo Jääskeläinen Tehtävä #.# Viikko #
-public class grafiikka {
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
 
+public class grafiikka extends JPanel {
+
+   
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.setColor(Color.RED);
+        g.drawString("START F1", 20, 20);
+        g.drawString("EXIT F2", 500, 20);
+        
+    }
 }
